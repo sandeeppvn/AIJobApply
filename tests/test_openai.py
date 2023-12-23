fdca3cf790b43b94f417d229e28f495222c6eea0
sandeeppvn <sandeeppvn@gmail.com>
2023-12-22 19:47:12 -0500

    changes

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -2,7 +2,7 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
-from src.openai_handler import OpenAIConnectorClass
+from src.llm_handler import OpenAIConnectorClass
 
 
 @pytest.fixture
